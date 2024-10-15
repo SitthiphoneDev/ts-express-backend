56ecda41d5feb5fe876f18e18d02fc4a7b61bd48
Sitthiphone <Sitthiphone.pv@gmail.com>
2024-10-16 00:24:34 +0700

    feat: add morgan  log

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,14 +1,18 @@
 import express from 'express';
+import morgan from 'morgan';
+
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API KUYYYYYYYYYYY SUSS' });
+  res.send({ message: 'Hello API Test Success' });
 });
 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
 });
+
